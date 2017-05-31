7e82b829bb331fabfc93e1e69ed40d8b16b91f57
thefunkygnome <thefunkygnome@gmail.com>
2017-05-31 22:51:05 +0300

    Logo, text slider and sharing update

---
+++ js/share-menu.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.share').click(function() {
+    if($('.social-likes').is(":hidden")) {
+      $('.social-likes').css('display','flex')
+      $('.share-menu').css('height','100%')
+    } else {
+      $('.social-likes').css('display','none')
+      $('.share-menu').css('height','20%')
+    }
+  })
+})
