4c67d51294a4a28b246972c7f675a993101b5ac4
thefunkygnome <thefunkygnome@gmail.com>
2017-06-20 22:16:51 +0300

    Getting things done

---
+++ js/menubar.js
@@ -0,0 +1,37 @@
+function open() {
+  $('.hamburger').hide()
+  $('.close').show()
+  $('.main-options').css('display','flex')
+  $('.contact').show()
+  $('.fade').show()
+  $('body').css('overflow', 'hidden')
+};
+
+function close() {
+  $('.hamburger').show()
+  $('.close').hide()
+  $('.main-options').css('display','none')
+  $('.contact').hide()
+  $('.fade').hide()
+  $('body').css('overflow', 'scroll')
+};
+
+$(function() {
+  $('.mobile-nav').click(function() {
+    if ($('.hamburger').is(':visible')) {
+      open()
+    } else {
+      close()
+    }
+    })
+
+    $('.fade').click(function() {
+      close()
+    })
+
+    // $('.contact').click(function() {
+    //   close()
+    // })
+
+
+  })
