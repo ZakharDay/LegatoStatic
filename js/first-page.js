284404f0e7e2bc6458b070e79c7fdc48d3b4cfcb
thefunkygnome <thefunkygnome@gmail.com>
2017-06-26 01:58:10 +0300

    Updated fadeout

--- js/first-page.js
+++ js/first-page.js
@@ -1,35 +1,16 @@
-
 $(function() {
-  // $(window).on('scroll', function() {
-  //   console.log('gogogo')
-  //
-  //   $('#faded').css('opacity','0');
-  //   $('.first-page-wrapper').toggleClass('transparent');
-  //   $('.menubar').toggleClass('menu-main');
-  // })
+  $(window).on('scroll', function() {
+    console.log('gogogo')
+    var scrollTop = $(this).scrollTop()
 
-  var firstScreen = $('.first-page-wrapper');
-  var range = 200;
-
-  $(window).on('scroll', function () {
-
-    console.log('im being scrolled')
-
-    $('#faded').css('opacity','0');
-
-    var scrollTop = $(this).scrollTop(),
-        height = firstScreen.outerHeight(),
-        offset = height / 2,
-        calc = 1 - (scrollTop - offset + range) / range;
-
-    firstScreen.css({ 'opacity': calc });
-
-    if (calc > '1') {
-      firstScreen.css({ 'opacity': 1 });
-      $('.menubar').addClass('menu-main');
-    } else if ( calc < '0' ) {
-      firstScreen.css({ 'opacity': .25 });
+    if (scrollTop > 0) {
+      $('#faded').css('opacity','0');
+      $('.first-page-wrapper').addClass('transparent');
       $('.menubar').removeClass('menu-main');
+    } else {
+      $('#faded').css('opacity','1');
+      $('.first-page-wrapper').removeClass('transparent');
+      $('.menubar').addClass('menu-main');
     }
-  });
+  })
 })
