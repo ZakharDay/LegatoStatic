561a8a0bc230a425c13d23cce05b49d3862058fb
ZakharDay <zakhar.day@gmail.com>
2017-07-03 04:03:02 +0300

    Experiment added

--- js/index.js
+++ js/index.js
@@ -9,5 +9,18 @@ $(function() {
       $('body').addClass('initial')
       $('video').get(0).play()
     }
+
+    $('figcaption').each(function(i) {
+      console.log(i, this.getBoundingClientRect().top, window.innerHeight)
+      var half = window.innerHeight / 2
+      var top = this.getBoundingClientRect().top
+      var height = this.getBoundingClientRect().height
+
+      if (top <= half && top + height > half) {
+        $(this).addClass('visible')
+      } else {
+        $(this).removeClass('visible')
+      }
+    })
   })
 })
