f33fa37bd18d1dcf08d7be3bf5fdc1b7155850e8
ZakharDay <zakhar.day@gmail.com>
2017-09-26 14:33:32 +0300

    Uncommited stuff

--- js/tagline.js
+++ js/tagline.js
@@ -2,7 +2,7 @@ $(function() {
   var counter = 0
 
   function move() {
-    if (counter >= 8) {
+    if (counter >= 6) {
       counter = 0
     }
 
@@ -15,23 +15,19 @@ $(function() {
       .removeClass('step5')
       .removeClass('step6')
       .removeClass('step7')
-      .removeClass('step8')
-      .removeClass('step9')
       .addClass('step' + counter)
     counter = counter + 1
   }
 
   function animateWithTimeout() {
     move()
-    setTimeout(move, 4000)
-    setTimeout(move, 8000)
+    setTimeout(move, 3000)
+    setTimeout(move, 6000)
+    setTimeout(move, 9000)
     setTimeout(move, 12000)
-    setTimeout(move, 16000)
-    setTimeout(move, 20000)
-    setTimeout(move, 24000)
-    setTimeout(move, 28000)
-    setTimeout(move, 32000)
-    setTimeout(animateWithTimeout, 38000)
+    setTimeout(move, 15000)
+    setTimeout(move, 18000)
+    setTimeout(animateWithTimeout, 26000)
   }
 
   animateWithTimeout()
